22d0d081a53d5fb146ef1c81ccdc241598c2f048
J-Okoto <okotojr@gmail.com>
2020-05-17 02:02:59 +0300

    form details submission alert incorporated

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-function Submit(){
-  alert("Thank you for sumittting your details");
-}
+$(document).ready(function() {
+$("form#form34A").submit(function(event) {
+
+          var name = $("input#MERGE1").val();
+          var email = $("input#MERGE0").val();
+          var message = $("textarea#comment").val();
+          if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+      alert (name + ", we have received your message. Thank you for submitting your details.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+
+  });
+
+});
