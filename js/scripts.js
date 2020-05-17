9922617f402d288063896256fa2ba244c4a5e93c
J-Okoto <okotojr@gmail.com>
2020-05-17 19:03:30 +0300

    JQuery adding ad removing classes functions in portfolio section in container3

--- js/scripts.js
+++ js/scripts.js
@@ -14,44 +14,92 @@ $("form#form34A").submit(function(event) {
   });
 
 });
-  
+
 
   $(document).ready(function(){
     $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
+      $("#development-image").hide();
+      $("#development").show();
     });
     $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
+      $("#development").hide();
+      $("#development-image").show();
     });
   });
 
   $(document).ready(function(){
     $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
-      $("#design").show('1500');
+      $("#design-image").hide();
+      $("#design").show();
     });
     $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#design-image").slideDown('1500');
+      $("#design").hide();
+      $("#design-image").show();
     });
   });
 
   $(document).ready(function(){
     $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
+      $("#product-image").hide();
+      $("#product").show();
     });
     $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
+      $("#product").hide();
+      $("#product-image").show();
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#overlay1").show();
+    })
+    .mouseout(function(){
+      $("#overlay1").hide();
     });
   });
 
   $(document).ready(function(){
-    $("#work1").hover(function(){
-      $("#overlay1").show();
-      });
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+});
 
+$(document).ready(function(){
+  $("#work5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+  $("#work6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+  $("#work7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+  $("#work8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
 });
